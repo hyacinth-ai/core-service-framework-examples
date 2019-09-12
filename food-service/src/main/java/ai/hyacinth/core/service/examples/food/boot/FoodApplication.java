e67888079d1a37fe1361cf0a910e14a658a7db33
Xu Yijun <xuyijun@gmail.com>
2019-09-13 00:10:09 +0800

    flyway testing

--- food-service/src/main/java/ai/hyacinth/core/service/examples/food/boot/FoodApplication.java
+++ food-service/src/main/java/ai/hyacinth/core/service/examples/food/boot/FoodApplication.java
@@ -4,13 +4,22 @@ import ai.hyacinth.core.service.examples.food.config.FoodApplicationConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({FoodApplicationConfig.class})
 @Slf4j
-public class FoodApplication {
+public class FoodApplication implements ApplicationListener<ApplicationReadyEvent> {
   public static void main(String[] args) {
     SpringApplication.run(FoodApplication.class, args);
+  }
+
+  @Override
+  public void onApplicationEvent(ApplicationReadyEvent event) {
+    String key = "spring.flyway.enabled";
+    String value = event.getApplicationContext().getEnvironment().getProperty(key);
+    log.info("property value: {} = {}", key, value);
   }
 }
